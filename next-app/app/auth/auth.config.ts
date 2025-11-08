ea27614d0d36d5465d8a5e1a2c47397a6eb9a534
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-11-08 13:00:32 -0800

    update(formatting): have terraform allow me to be lazy

--- next-app/app/auth/auth.config.ts
+++ next-app/app/auth/auth.config.ts
@@ -1,20 +1,20 @@
-const AWS_REGION = process.env.NEXT_PUBLIC_AWS_REGION || "us-east-1";
+const SCOPES = "email openid"
 
 export const cognitoAuthConfig = {
-  authority: `https://cognito-idp.${AWS_REGION}.amazonaws.com/${process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID}`,
+  authority: process.env.NEXT_PUBLIC_COGNITO_ENDPOINT,
   client_id: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID!,
   redirect_uri: process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URI!,
   response_type: "code",
-  scope: "phone openid email",
+  scope: SCOPES,
   onSigninCallback: () => {
     window.history.replaceState({}, document.title, window.location.pathname);
   },
 };
 
 export const cognitoLogoutConfig = {
-  domain: `https://${process.env.NEXT_PUBLIC_COGNITO_DOMAIN}.auth.${AWS_REGION}.amazoncognito.com`,
+  domain: process.env.NEXT_PUBLIC_COGNITO_DOMAIN,
   clientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID!,
   redirect_uri: process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URI!,
   response_type: "code",
-  scope: "email openid phone",
+  scope: SCOPES,
 };
