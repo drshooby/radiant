88e012fba8a3ddeb4eee1208cc7d095ce48791a2
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-11-10 13:00:34 -0800

    chore: note for removing CORS *

--- infra/aws/api_gateway.tf
+++ infra/aws/api_gateway.tf
@@ -85,7 +85,7 @@ resource "aws_api_gateway_integration_response" "cognito_options" {
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
     "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'" # update this
   }
 }
 
