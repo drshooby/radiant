ad7422e631b95a20bae0ecba8fef70ef7b4e2db8
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-11-14 13:56:58 -0800

    add: ffmpeg layer

---
+++ infra/aws/ffmpeg_lambda_layer.tf
@@ -0,0 +1,11 @@
+resource "aws_lambda_layer_version" "ffmpeg_layer" {
+  filename         = "${path.module}/lambda/ffmpeg/ffmpeg.zip"
+  layer_name       = "ffmpeg"
+  description      = "FFMPEG layer for lambda video processing"
+  license_info     = "MIT"
+  source_code_hash = filebase64sha256("${path.module}/lambda/ffmpeg/ffmpeg.zip")
+
+  compatible_runtimes = ["python3.12"]
+
+  compatible_architectures = ["x86_64"]
+}
